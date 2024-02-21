e2e69dbd1210992e0e4b918dc1ba9c0c3fdc040d
mikailacomfort <mcomfort@Mikailas-MacBook-Air.local>
2024-02-20 20:32:48 -0600

    update

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-let arrayOfTodos = [];
+let arrayOfTodos = [
+  {
+  "userId": 14,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+},
+{
+  "userId": 20,
+  "id": 2,
+  "title": "delectus aut autem",
+  "completed": false
+}];
 
 const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
